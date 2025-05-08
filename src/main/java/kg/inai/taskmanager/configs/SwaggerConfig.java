8a7e02f98dac40adbea0a7dc819c84448e0a30b2
ajzatmadumarova <aizatmadumarova04@gmail.com>
2025-05-08 21:47:58 +0600

    Added Swagger Bearer auth config

---
+++ src/main/java/kg/inai/taskmanager/configs/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package kg.inai.taskmanager.configs;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "Bearer Authentication";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName)
+                )
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, createSecurityScheme())
+                )
+                .info(new Info()
+                        .title("Task Manager API")
+                        .version("v1"));
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+    }
+}
